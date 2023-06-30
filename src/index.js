cfe8a5dcffec6966660fb30fc5bb8592dd181fbd
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-06-30 12:20:57 +0300

    Update basename in router index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <BrowserRouter basename="/money-guard">
+  <BrowserRouter basename="/react_bc53_getchagrooveon">
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <PersistGate persistor={persistor}>
